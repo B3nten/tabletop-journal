a3f8bdfa3e697a0287c2861682a251953126efe4
benten <ben@benten.ca>
2022-05-01 13:46:51 -0600

    added return home button

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,13 @@
 import {Navigation} from "../components/Navigation"
+import Link from "next/link"
 
 export default function notFound(){
 
     return (
         <div>
-            <Navigation title='Page Not Found' />
-            <img className='w-full' src='/404.png'/>
+            <Navigation title='404 Page Not Found' />
+            <Link href='/'><button className='btn-underline'>return home</button></Link>
+            <img alt="A dragon and archers fighting unseen foes." className='w-full' src='/404.png'/>
         </div>
     )
 }
