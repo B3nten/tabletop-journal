1326b578a5afb580c9bca9bde9cd6824da626682
benten <ben@benten.ca>
2022-05-05 16:33:57 -0600

    cleaned up auth flow

--- components/AuthGuard/AuthCheck.js
+++ components/AuthGuard/AuthCheck.js
@@ -1,12 +1,13 @@
 import { useSession } from "../../lib/authentication"
 import Login from "../Login/Login"
-import Loader from '../Loader/Loader'
+import { useMounted } from "../../lib/hooks"
 
 export function AuthCheck(props) {
     const session = useSession()
+    const mounted = useMounted
 
     if(session && session.loading){
-        return <Loader />
+        return null
     }else if (session){
         return <>{props.children}</>
     }else{
--- pages/campaigns/index.js
+++ pages/campaigns/index.js
@@ -1,47 +1,54 @@
-import Link from "next/dist/client/link"
+import Link from 'next/dist/client/link'
 import Loader from '../../components/Loader/Loader'
 import Navigation from '../../components/Navigation/Navigation'
-import { AuthCheck } from "../../components/AuthGuard"
-import { useCampaignList } from "../../lib/database"
-import { Metatags } from "../../components/Metatags"
-
-
+import { AuthCheck } from '../../components/AuthGuard'
+import { useCampaignList } from '../../lib/database'
+import { Metatags } from '../../components/Metatags'
 
 export default function Campaigns() {
-    const campaigns = useCampaignList()
-    
-    return (<>
-            <Metatags title='Campaigns'/>
-            <Navigation title='Campaigns' />
-            <AuthCheck>
-                {!campaigns.loaded && <Loader centred />}
-                {campaigns.loaded && 
-                <div className='flex flex-col items-center space-y-16'>
-                    <CampaignList campaigns={campaigns.list} />
-                    <Link href='/campaigns/create'>
-                        <button className='btn-underline hover:scale-105'>Create Campaign</button>
-                    </Link>
-                </div>}
-            </AuthCheck>
-        </>)
+  const campaigns = useCampaignList()
+
+  return (
+    <>
+      <Metatags title='Campaigns' />
+      <Navigation title='Campaigns' />
+      <AuthCheck>
+        {!campaigns.loaded && <Loader centred />}
+        {campaigns.loaded && (
+          <div className='flex flex-col items-center space-y-16'>
+            <CampaignList campaigns={campaigns.list} />
+            <Link href='/campaigns/create'>
+              <button className='btn-underline hover:scale-105'>
+                Create Campaign
+              </button>
+            </Link>
+          </div>
+        )}
+      </AuthCheck>
+    </>
+  )
 }
 
 function CampaignList(props) {
-    return (
-        <div className='flex flex-col items-center space-y-8'>
-            {props.campaigns?.length === 0 ? 'No campaigns yet.'
-                : props.campaigns?.map(campaign =>
-                    <CampaignItem key={campaign.id} campaign_id={campaign.campaign_id} name={campaign.name} />)
-            }
-        </div>
-    )
+  return (
+    <div className='flex flex-col items-center space-y-8'>
+      {props.campaigns?.length === 0
+        ? 'No campaigns yet.'
+        : props.campaigns?.map((campaign) => (
+            <CampaignItem
+              key={campaign.id}
+              campaign_id={campaign.campaign_id}
+              name={campaign.name}
+            />
+          ))}
+    </div>
+  )
 }
 
 function CampaignItem(props) {
-    return (
-        <Link href={`/campaigns/${props.campaign_id}`}>
-            <button className='btn-primary'>{props.name}</button>
-        </Link>
-    )
+  return (
+    <Link href={`/campaigns/${props.campaign_id}`}>
+      <button className='btn-primary'>{props.name}</button>
+    </Link>
+  )
 }
-
