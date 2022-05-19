bee41cd105732ca01dec7e0d0701c820e1e02191
benten <ben@benten.ca>
2022-05-18 23:54:53 -0600

    changed description to overview

--- components/Document/Document.js
+++ components/Document/Document.js
@@ -2,32 +2,32 @@ import { AuthCheck } from "../AuthGuard"
 import { String, Richtext } from '../../components/Fields'
 import Loader from "../Loader/Loader"
 
-export default function DocumentPage({document}) {
+export default function DocumentPage({ document }) {
 
-    if(!document.loaded) return <Loader />
-    if(document.loaded && !document.isData) return <div>Document not found.</div>
-    
+    if (!document.loaded) return <Loader />
+    if (document.loaded && !document.isData) return <div>Document not found.</div>
+
     return (
-        <div>
+        <div className='w-full'>
             <AuthCheck>
-                    <div className="space-y-5">
-                            <String
-                                value={document.name}
-                                id={document.id}
-                                title='Name'
-                                placeholder='document name'
-                                from='documents'
-                                column='name'
-                            />
-                        <Richtext
-                            id={document.id}
-                            title='Description'
-                            content={document.description?.content}
-                            from='documents'
-                            columns={['description', 'description_plaintext']}
-                             />
-                        
-                    </div>
+                <div className="space-y-5 w-full">
+                    <String
+                        value={document.name}
+                        id={document.id}
+                        title='Name'
+                        placeholder='document name'
+                        from='documents'
+                        column='name'
+                    />
+                    <Richtext
+                        id={document.id}
+                        title='Description'
+                        content={document.overview?.content}
+                        from='documents'
+                        columns={['overview', 'overview_plaintext']}
+                    />
+
+                </div>
             </AuthCheck>
         </div>
     )
--- components/Document/DocumentList.js
+++ components/Document/DocumentList.js
@@ -10,13 +10,17 @@ export default function DocumentList(props) {
 	return (
 		<>
 			{props.documents.map((doc) => (
-				<div className='group' key={doc.id}>
+				<div className='' key={doc.id}>
 					<Link href={`/campaigns/${router.query.campaign}/${page}/${doc.id}`}>
 						<a className='block text-2xl font-handwriting leading-none transition-all space-y-1'>
-							<div>{doc.name}</div>
+							<div className='relative inline-block group'>{doc.name}
+								<div className='absolute inset-x-0 h-[1px] scale-x-0 group-hover:scale-x-100 transition-all origin-left'>
+									<img className='w-full h-[2px]' src='/bottom_line.svg'></img>
+								</div>
+							</div>
 							<div className='flex items-center space-x-4 '>
 								<div className='text-sm leading-none'>{doc.type}</div>
-								<div className='flex items-center space-x-2'>
+								<div className='flex items-center space-x-2 hidden'>
 									<img src='/icons/pin.svg' className='w-5 h-5 scale-0 group-hover:scale-100 transition-all'></img>
 									<img src='/icons/gear.svg' className='w-4 h-4 opacity-70 scale-0 group-hover:scale-100 transition-all'></img>
 								</div>
