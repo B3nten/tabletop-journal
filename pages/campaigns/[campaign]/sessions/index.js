ff480b8f4b80179530c8650764d3803d2a656b91
benten <ben@benten.ca>
2022-05-19 00:08:38 -0600

    useGameSessionList name change

--- pages/campaigns/[campaign]/sessions/index.js
+++ pages/campaigns/[campaign]/sessions/index.js
@@ -1,7 +1,8 @@
 import { AuthCheck } from '../../../../components/AuthGuard'
 import Loader from '../../../../components/Loader/Loader'
 import { useClientRouter } from '../../../../lib/hooks'
-import { useSessionList } from '../../../../lib/database'
+import { useGameSessionList } from '../../../../lib/database'
+import Navigation from '../../../../components/Navigation/Navigation'
 
 
 export default function SessionsPage() {
@@ -16,19 +17,17 @@ export default function SessionsPage() {
 
 function DocumentList() {
     const router = useClientRouter()
-    const sessions = useSessionList(router.query.campaign)
+    const sessions = useGameSessionList(router.query.campaign)
 
     return (<>
+        <Navigation title='Sessions'/>
         <div className=''>
             <div>
-                <div className='mb-10'>
-                    <h2 className='font-fancy text-6xl'>Sessions</h2>
-                </div>
                 {sessions.loaded ? sessions.data?.length > 0 ?
-                    sessions.data.map((d) =>
+                    sessions.data.map((session) =>
                         <DocumentListItem
-                            key={d.session_id}
-                            id={d.session_id}
+                            key={session.id}
+                            session={session}
                         >
                         </DocumentListItem>)
                     : <div className='font-handwriting opacity-80 pl-1'>no items</div>
@@ -42,8 +41,8 @@ function DocumentList() {
 function DocumentListItem(props) {
     const router = useClientRouter()
     return (
-        <div onClick={() => router.push(`/campaigns/${router.query.campaign}/sessions/${props.id}`)} className='flex flex-col items-start mt-8 space-y-1 cursor-pointer hover:scale-[1.02] transition-all'>
-            <div className='text-3xl font-handwriting'>{props.id}</div>
+        <div onClick={() => router.push(`/campaigns/${router.query.campaign}/sessions/${props.session.id}`)} className='flex flex-col items-start mt-8 space-y-1 cursor-pointer hover:scale-[1.02] transition-all'>
+            <div className='text-3xl font-handwriting'>Session {props.session.number}</div>
             <img className='w-full h-1 opacity-20' src='/bottom_line.svg' alt='' />
         </div>
     )
