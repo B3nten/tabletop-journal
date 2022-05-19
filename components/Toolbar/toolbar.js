88d59ac2725afd6ef171d0cb2dd2e8d5f4914ea7
benten <ben@benten.ca>
2022-05-19 00:07:18 -0600

    added link to sessions

--- components/Toolbar/toolbar.js
+++ components/Toolbar/toolbar.js
@@ -2,16 +2,22 @@ import { useSession } from '../../lib/authentication'
 import { useClientRouter } from '../../lib/hooks'
 import styles from './toolbar.module.css'
 import Link from 'next/link'
+import { useCampaign } from '../../lib/database'
 
 export default function Toolbar() {
   const session = useSession()
   const router = useClientRouter()
 
   // !session.loading prevents hydration errors, could also check if mounted
-  if (session && !session.loading && router.query.campaign) {
+  if (session && !session.loading && router.query?.campaign ) {
     return (
       <div className={styles.wrapper}>
         <div className={styles.toolbar}>
+        <Link href={`/campaigns/${router.query.campaign}/sessions`}>
+            <button className={styles.button}>
+              <img src='/icons/sessions.svg' title='Sessions' />
+            </button>
+          </Link>
           <Link href={`/campaigns/${router.query.campaign}/characters`}>
             <button className={styles.button}>
               <img src='/icons/characters.svg' title='Characters' />
@@ -35,6 +41,11 @@ export default function Toolbar() {
           <Link href={`/campaigns/${router.query.campaign}/dreams`}>
             <button className={styles.button}>
               <img src='/icons/dreams.svg' title='Dreams' />
+            </button>
+          </Link>
+          <Link href={`/campaigns/${router.query.campaign}/events`}>
+            <button className={styles.button}>
+              <img src='/icons/events.svg' title='Events' />
             </button>
           </Link>
           <Link href={`/campaigns/${router.query.campaign}/other`}>
